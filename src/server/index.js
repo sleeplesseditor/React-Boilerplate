d21aa299516e4c6cdc51a695cf68a7c8a4b633ed
sleeplesseditor <josh@bryson.me.uk>
2018-01-29 14:42:39 +0000

    App finalised

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,4 @@ let webServer = new WebServer();
 webServer.start()
     .then(() => {
         console.log('Web server started! Listening on port 3000')
-    })
-    .catch(err => {
-        console.error(err)
-        console.error('Failed to start web server')
     });
--- src/server/web.server.test.js
+++ src/server/web.server.test.js
@@ -1,19 +1,19 @@
-import WebServer from './web.server';
+import WebServer from './web.server'
 
 describe('Started', () => {
-    let webServer = null
+  let webServer = null
 
-    beforeAll(() => {
-        webServer = new WebServer()
-    })
+  beforeAll(() => {
+    webServer = new WebServer()
+  })
 
-    test('should start and trigger a callback', async () => {
-        let promise = webServer.start()
-        await expect(promise).resolves.toBeUndefined()
-    })
+  test('should start and trigger a callback', async () => {
+    let promise = webServer.start()
+    await expect(promise).resolves.toBeUndefined()
+  })
 
-    test('should stop and trigger a callback', async () => {
-        let promise = webServer.stop()
-        await expect(promise).resolves.toBeUndefined()
-    })
+  test('should stop and trigger a callback', async () => {
+    let promise = webServer.stop()
+    await expect(promise).resolves.toBeUndefined()
+  })
 })
