cb89bf2531b695952787bc9136479cbfd71bfb1a
sleeplesseditor <josh@bryson.me.uk>
2018-01-29 11:00:43 +0000

    server message updated

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ import WebServer from './web.server';
 let webServer = new WebServer();
 webServer.start()
     .then(() => {
-        console.log('Web server started!')
+        console.log('Web server started! Listening on port 3000')
     })
     .catch(err => {
         console.error(err)
